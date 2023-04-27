2f0823060c271db71b91af9bedadf068ed9358b3
Fahri <cfaxri@mail.ru>
2023-04-27 12:49:27 +0000

    Revert "change"
    This reverts commit 0a34c8b90e315da6f4aa48fccd11bd1e00a9c5c8

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         MainFlow mainFlow = new MainFlow();
         mainFlow.start();
-        System.out.println("Salam");
     }
 }
