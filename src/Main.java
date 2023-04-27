0a34c8b90e315da6f4aa48fccd11bd1e00a9c5c8
zeynepbektas <zeynepbektas>
2023-04-27 15:27:19 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         MainFlow mainFlow = new MainFlow();
         mainFlow.start();
+        System.out.println("Salam");
     }
 }
